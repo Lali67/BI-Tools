21177c51a81f6806d0c7a5f28469895c11209120
Lali67 <lajos67.szoke@gmail.com>
2019-12-02 21:50:04 +0100

    Delete 07-Sql-Insurance_sum_total_depth_cover_premium script.sql

--- Qlikview/Course-Udemy-Boot Camp/Section02-Data Modelling In Qlikview/07-Sql-Insurance_sum_total_depth_cover_premium script.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-	m.gender
-	,sum(total_death_cover_premium) AS TotalDeathCoverByGender
-FROM
-	dbo.dimMember m INNER JOIN
-	dbo.dimMemberCover mc
-ON	m.MemberKey = mc.MemberKey
-GROUP BY 
-	m.gender;
